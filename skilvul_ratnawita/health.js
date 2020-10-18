f5a764d63d08654b66b7ac2eb604a7860d1ea92d
ratnawita <63650439+ratnawita@users.noreply.github.com>
2020-10-18 21:02:49 +0700

    Add files via upload

---
+++ skilvul_ratnawita/health.js
@@ -0,0 +1,11 @@
+const contact-form = document.querySelector('.contact-form');
+const email = document.querySelector('.email');
+const pesan = document.querySelector('.pesan');
+
+	if (email != '' && pesan != '') {
+		register.addEventListener('submit', function(){
+			console.log('Pesan Anda berhasil terkirim');
+		})
+	}
+
+	
